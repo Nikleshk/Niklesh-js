45064e601011a5202bedaba5fcb81e4d19966472
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-01 18:04:01 +0000

    one line updated

--- basics/variable.js
+++ basics/variable.js
@@ -2,8 +2,9 @@ const collegeId=34342
 let name="niklesh"
 var course="csm"
 city="ibp"
+let accountState;
 /*prefer not to use var
 because it cause block scope issues and functional issues
 */
 console.log(city);
-console.table([collegeId,name,course,city])
+console.table([collegeId,name,course,city,accountState])
