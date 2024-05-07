aef4115e5dde980cd45fed26cd210950b8a19273
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-07 15:46:35 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+let name="Niklesh"
+let age=21
+let isLoggedIn=true
+/*primitive data types
+number=> 2 to the power 53
+bigint
+string=> ""
+boolean=> true/false
+null=> standalone value
+undefined => undefined
+symbol=> unique
+*/
+
+//object data type
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
