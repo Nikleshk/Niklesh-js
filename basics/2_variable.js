d7480df8d0c131d1207f00840a793e5b9f7bd529
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-09 07:58:49 +0000

    renamed all files

---
+++ basics/2_variable.js
@@ -0,0 +1,10 @@
+const collegeId=34342
+let name="niklesh"
+var course="csm"
+city="ibp"
+let accountState;
+/*prefer not to use var
+because it cause block scope issues and functional issues
+*/
+console.log(city);
+console.table([collegeId,name,course,city,accountState])
---
+++ basics/4_conversionoperation.js
@@ -0,0 +1,15 @@
+let score="33";
+ 
+console.log(typeof score);
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => number=33
+//"22abc"=> NaN
+//true=1,false=0
+
+let isLoggedIn =1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
