c9a8ea5f5de281e2bf09f4c5e2e2f493f9ccb374
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-09 08:05:30 +0000

    edit

--- basics/4_conversionoperation.js
+++ basics/4_conversionoperation.js
@@ -13,3 +13,10 @@ console.log(valueInNumber);
 let isLoggedIn =1
 let booleanIsLoggedIn= Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+
+//1=>true 0=>false
+//""=>false "niklesh"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
