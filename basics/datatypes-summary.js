e952702d8baae2abbbac841672debca595658286
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-14 12:33:23 +0000

    updated

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -24,12 +24,35 @@ console.log( id === courseId );
 
 const heros=["shaktimaan","Naagraj","Powerman"];
 
-let myObj={
+let hisObj = {
     name:"hitesh",
-    age:20,
+    age:'20',
 }
 
 const myFunction =function(){
     console.log("hello world");
 }
 console.log(typeof heros);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack and heap memory
+//Stack memory fetches the most recent or updated ..
+let myYoutubeName = "nikleshk800@gmail.com"
+let anotherYoutubeName=myYoutubeName
+anotherYoutubeName="nk@gmail.com"
+
+console.log(myYoutubeName);
+console.log(anotherYoutubeName);
+
+//heap memory is a whole memory while stack is a step by step memory
+
+let myObj = {
+    id:"nikleshk800@gmail.com",
+    course:"csm"
+}
+
+let ourObj=myObj
+ourObj.id="nk@gmail.com"
+console.log(ourObj.id);
+console.log(myObj.id);
