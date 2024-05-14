d3787d268bd64f317bbf72f01581437dd4ee4f13
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-14 08:19:09 +0000

    datatypes

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive 
+
+// 7 types:Number, Boolean ,  Symbol , String , BigInt, Null , Undefined
+const score=100
+const scoreValue=1000.8
+
+const isLoggedIn=false
+const isArrived=true
+
+
+
+let userEmail;
+
+const id = Symbol('123')
+const courseId=Symbol('123')
+
+console.log( id === courseId );
+
+//const bigNumber=1231143217842n
+
+// Reference (Non primitive)
+
+//Arrays,Objects,Functions
+
+const heros=["shaktimaan","Naagraj","Powerman"];
+
+let myObj={
+    name:"hitesh",
+    age:20,
+}
+
+const myFunction =function(){
+    console.log("hello world");
+}
+console.log(typeof heros);
