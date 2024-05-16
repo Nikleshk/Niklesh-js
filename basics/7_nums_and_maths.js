265ca5f010d70d604dd2ee7f493dbc8ad18e5ec3
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-16 08:09:16 +0000

    nums and maths

---
+++ basics/7_nums_and_maths.js
@@ -0,0 +1,27 @@
+const value = new Number(1000)
+//console.log(value);
+//console.log(value.toFixed(2));
+
+const gpa=new Number(93.86)
+//console.log(gpa.toPrecision(3));
+
+
+const salary =1000000
+//console.log(salary.toLocaleString('en-IN'));
+
+//////////////////////////+++++maths++++++////////////////////////////////////
+console.log(Math);
+console.log(Math.ceil(4.3));
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
+
+console.log(Math.random())
+console.log(Math.random()*10);
+console.log(Math.random()*10 + 1);
+console.log(Math.floor(Math.random()*10 + 1));
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1) + min)); 
