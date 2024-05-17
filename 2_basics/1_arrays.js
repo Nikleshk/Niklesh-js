377059f772249c97ec943e959e8ec5c55725dac1
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-17 12:19:46 +0000

    arrays part a

---
+++ 2_basics/1_arrays.js
@@ -0,0 +1,24 @@
+//array
+
+const myArr =[1,2,3,4,5,6];
+const heroes = ["shaktimaan","batman","superman"]
+console.log("A",myArr);
+
+//////////////////methods/////////////////
+//myArr.push(6)
+// myArr.pop()
+// myArr.unshift(6)
+// myArr.shift(6)
+// console.log(myArr.includes(9));
+// console.log(myArr.includes(3));
+
+// console.log(myArr);
+const newArr = myArr.join()
+// console.log(typeof newArr);
+// console.log(newArr);
+
+const myn = myArr.slice(1,5)
+
+console.log("B",myn);
+const mynn = myArr.splice(1,5)
+console.log("C",mynn);
