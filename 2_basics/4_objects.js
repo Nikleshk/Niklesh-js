97158887db5df31c90d86730368561a36cadb9ec
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-05-23 13:33:43 +0000

    objects2

---
+++ 2_basics/4_objects.js
@@ -0,0 +1,31 @@
+//const tinderUser = new object()
+
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "niklesh"
+tinderUser.isLoggedIn = "false"
+ //console.log(tinderUser);
+//  console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser))
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+ const regularUser = {
+    fullName :{
+        firstName:"niklesh",
+        secondName:"kumar"
+
+    }
+ }
+ //console.log(regularUser.fullName.firstName);
+
+
+ const id1 = {1:"a",2:"b"}
+ const id2 = {3:"c",4:"d"}
+ const id4 = {5:"e",6:"f"}
+ //const id3 = {id1,id2}
+ const id3 = Object.assign({},id1,id2,id4)
+ //console.log(id3);
+ const id6 = {...id1,...id2,...id4}
+ console.log(id6);
