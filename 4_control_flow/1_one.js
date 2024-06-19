90e0005ee20affb72bb45d83b5c7af5326388f47
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-06-19 15:35:14 +0000

    control flow

---
+++ 4_control_flow/1_one.js
@@ -0,0 +1,49 @@
+// if(2!=2){
+//     console.log(`doesn't equal to ${2}`);
+// }
+// else{
+//     console.log(`it is equal to ${2}`);
+// }
+
+// const temperature = 51;
+// if(temperature===51){
+//     console.log("overheat");
+// }
+// else{
+//     console.log("moderate");
+// }
+// console.log("!!!weather report!!!");
+
+// const score = 200;
+// if(score > 100){
+//     const partnership='111';
+//     console.log(`easy win with partnership ${partnership}`);
+
+// }
+// console.log(partnership);
+//const is defined to respective scope only, where var is globally scoped//
+
+
+
+// const price = 1000;
+// if (price <500) {
+//     console.log("less than 500");
+    
+// }else if(price <600){
+//     console.log("less than 600");
+// }else if (price<700){
+//     console.log("less than 700");
+// }else {
+//     console.log("not enough");
+// }
+
+const userLoggedIn = true;
+const loggedInUsingGoogle = false;
+const loggedInUsingEmail = true;
+
+// if(userLoggedIn && loggedInUsingEmail){
+//     console.log("logged in succesfully")
+// }
+// if(userLoggedIn || loggedInUsingGoogle || loggedInUsingEmail){
+//     console.log("logged in ")
+// }
