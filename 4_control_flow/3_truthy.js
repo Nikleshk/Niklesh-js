9754fde9e8cb33bb624e5a7afd5313ad84f47be7
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-06-20 01:19:39 +0000

    truthy of falsy values

---
+++ 4_control_flow/3_truthy.js
@@ -0,0 +1,31 @@
+ 
+ const email = "niklesh";
+//  if(email){
+//     console.log("email available");
+//  }else{
+//     console.log("not available");
+//  }
+ //FALSY VALUES : "",0,-0,BigInt(0n),false,undefined,NaN,Null
+
+ //TRUTHY VALUES:" "(string with a space),"0",{},[],'false',function(){}
+
+//  const empObj = {};
+//  if(Object.keys(empObj).length==0){
+//     console.log("no keys");
+//  }
+
+let val1;
+// val1 = null ?? 10;
+// val1 = 10 ?? 20;
+// val1 = undefined ?? 5;
+// val1 = null ?? 10 ?? 15;
+// val1 = 10 ?? null;
+// val1 = undefined ?? 10;
+// console.log(val1);
+
+//TERNARY OPERATOR
+//SYNTAX = CONDITION ? TRUE : FALSE
+
+const value = 100;
+value <=80 ? console.log("value is matched") :
+console.log("value isn't matched");
