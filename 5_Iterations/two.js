8fffbfa7fe819c1f7835a74dc8173d159971cc9e
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-07-26 06:37:53 +0000

    while do while

---
+++ 5_Iterations/two.js
@@ -0,0 +1,21 @@
+// let index =0;
+// while (index<10) {
+//     console.log(`value is ${index}`);
+//     index = index+2
+// }
+
+// let myArray = ["superman","batman","spiderman"];
+// let arr =0;
+// while (arr<myArray.length) {
+//     console.log(myArray[arr]);
+//     arr = arr+1;
+// }
+
+
+
+let score=0;
+do {
+    
+    console.log(score);
+    score=score+1
+} while (score<10);
