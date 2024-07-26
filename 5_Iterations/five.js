27c759c696dd93f38f54444ddaf916ecf2abf14d
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-07-26 12:08:45 +0000

    basicx

---
+++ 5_Iterations/five.js
@@ -0,0 +1,19 @@
+const coding=["js","cpp","py","c","java"]
+// coding.forEach(function (val){
+//     console.log(val);
+// })
+
+
+//coding.forEach((item)=>(console.log(item)))
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+
+
+coding.forEach((item,index,Array)=>{
+    console.log(item,index,Array);
+}
+)
