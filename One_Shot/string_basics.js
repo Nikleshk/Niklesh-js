d62ef20d2be905cbe13e6c1ede8fe8060c31ef6c
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2024-11-29 16:21:00 +0000

    n

---
+++ One_Shot/string_basics.js
@@ -0,0 +1,14 @@
+var myName= 'Niklesh';
+var lastName="Kumar";
+
+
+
+var experiment = `i dont know \"what to do \"you should\"do my work\" also and help me`;
+var exp = 'i dont know "what to do "you should"do my work" also and help me';
+var exper = `i dont know "what to do "you should"do my work" also and help me`;
+console.log(myName);
+console.log(lastName);
+
+console.log(experiment);
+console.log(exp);
+console.log(exper);
