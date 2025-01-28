1dd67c196daec7a95867297803bf087430a97f94
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-01-28 16:05:10 +0000

    new

--- One_Shot/4_array.js
+++ One_Shot/4_array.js
@@ -1,2 +1,7 @@
 var anArray  = [ 'a boy' , 19 ]
 console.log(anArray);
+
+// Nested Array
+
+var nestedArray = [[ "acer", 12 ], ["asus", 11], ["acer", "asus", "hp",123,456]]
+console.log(nestedArray[2]);
