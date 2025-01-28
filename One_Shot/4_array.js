3f8da85f89a31ee09035951f222dd88bb3edadac
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-01-28 16:24:57 +0000

    hhh

--- One_Shot/4_array.js
+++ One_Shot/4_array.js
@@ -2,6 +2,18 @@ var anArray  = [ 'a boy' , 19 ]
 console.log(anArray);
 
 // Nested Array
-
+var nestedexample = [ "computer","laptop" ]
 var nestedArray = [[ "acer", 12 ], ["asus", 11], ["acer", "asus", "hp",123,456]]
 console.log(nestedArray[2]);
+console.log(nestedexample);
+
+
+var nest = [[12,122,111],[121,11,67],[[34],[23]]]
+console.log(nest[2][1]);
+
+
+// updating an array
+
+var array = [12,23,214];
+array[0] = 1;
+console.log(array);
