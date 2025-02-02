95fb58d4e859a19f94da7c642cbff0e4c4e474e7
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-02 12:32:26 +0000

    m

---
+++ One_Shot/10_switchcase.js
@@ -0,0 +1,41 @@
+function switchCase(value){
+    var result = "";
+    switch(value){
+        case 1:
+            result = "alpha";
+            break;
+        case 2 :
+            result = "beta";
+            break;
+        case 3:
+            result = "gamma";
+            break;
+        case 4 :
+            result = "delta";
+            break;       
+    }
+    return result;
+
+}
+console.log(switchCase(1));
+
+//Example using avengers
+function avengersByRank(rank){
+    var avenger ="";
+    switch(rank){
+        case 1 :
+            avenger="Thor";
+            break;
+        case 2:
+            avenger="Hulk";
+            break;
+        case 3:
+            avenger="Scarlet Witch";
+            break;
+        case 4 :
+            avenger="captain";
+            break;        
+    }
+    return avenger;
+}
+console.log(avengersByRank(1));
