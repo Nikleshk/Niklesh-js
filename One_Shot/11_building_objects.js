e2e7bc529a0e1aa7413117a83fd930c6dc60acbf
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-03 15:20:57 +0000

    j

---
+++ One_Shot/11_building_objects.js
@@ -0,0 +1,10 @@
+var myHouse={
+    "rooms":6,
+    "floors":2,
+    "type":"duplex",
+    "amenities":['elevator']
+};
+console.log(myHouse.type);
+console.log(myHouse);
+var valueOfHouse= myHouse.rooms;
+console.log(valueOfHouse);
