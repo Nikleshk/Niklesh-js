d5ce3f5605788dca8b7c0db24ca4e7bd078eda1a
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-08 14:57:10 +0000

    m

---
+++ One_shot-2/2_ternary_operator.js
@@ -0,0 +1,16 @@
+function checkEqual(a,b){
+    if(a===b){
+        return "true";
+        
+    }else {
+    return "false";
+}
+}
+console.log(checkEqual(1,1));
+
+//Example
+
+function checkEqual2(a,b){
+    return a === b ? true : false;
+}
+console.log(checkEqual2(1,1));
