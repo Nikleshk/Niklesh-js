19a2479d3bc86972ddba762b2c546049e0b6ce24
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-08 17:42:21 +0000

    m

--- One_shot-2/1_parseInt.js
+++ One_shot-2/1_parseInt.js
@@ -1,9 +1,4 @@
-function convertIntoInteger(str){
-     return parseInt(str);
+function convert(str){
+    return parseInt(str);
 }
-console.log(convertIntoInteger("56"));
-
-//Example 
-
-var converting ="1";
-console.log(parseInt(converting));
+console.log(convert("66"));
