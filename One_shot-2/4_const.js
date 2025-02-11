7d35c5c27f4ef3b369f71a5341941f1c5f389b67
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-11 17:41:18 +0000

    ,

--- One_shot-2/4_const.js
+++ One_shot-2/4_const.js
@@ -5,3 +5,17 @@ function primary(str){
     }
 }
 primary("FreeCodeCamp")  
+
+//example
+
+const s = [2,3,4];
+function editInPlace(){
+    s[0]=1;
+    s[1]=2;
+    s[2]=3;
+}
+editInPlace();
+console.log(s);
+
+
+//
