10c5cb09ae3eeccb4edc3ecc92306ceb6a852482
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-11 12:53:53 +0000

    m

--- One_shot-2/4_const.js
+++ One_shot-2/4_const.js
@@ -1,7 +1,7 @@
 function primary(str){
-    var sentence = str + "is cool";
-    sentence = str + "is amazing";
-    for(var i =0;i<str.length;i++){
+    var sentence = str + " is best";
+    sentence = str + " is awesome";
+    for(var i =0;i<str.length;i+=2){
         console.log(sentence);
         
     }
