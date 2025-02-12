e9bc7988c26f682030d0c41f0157a95b39b660b3
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-12 14:46:17 +0000

    m

--- One_shot-2/6_arrow_function.js
+++ One_shot-2/6_arrow_function.js
@@ -19,3 +19,25 @@ console.log(arrays([1,2],[3,4,5]));
 
 var arrays = (arr1,arr2)=>arr1.concat(arr2);
 console.log(arrays([1,2],[4,5]));
+
+
+
+//complex problems using arrow functions
+
+const anArray = [1,5,76,-4,1.5,34.5];
+
+const squaredNums= (arr)=>{
+    const squaredInts = arr.filter(num => Number.isInteger(num) && num>0).map(x=>x*x);
+    return squaredInts;
+}
+const squaredInts = squaredNums(anArray);
+
+console.log(squaredInts);
+
+
+//Increment function
+function incrementNums(num,value){
+   return num+value;
+}
+console.log(incrementNums(5,2));
+
