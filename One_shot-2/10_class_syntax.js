2d5b3f6e541a9653e812baba16326a7312c674eb
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-16 10:40:01 +0000

    xd

--- One_shot-2/10_class_syntax.js
+++ One_shot-2/10_class_syntax.js
@@ -3,3 +3,13 @@ var aPlanet=function(likedPlanet){
 }
 var that =new aPlanet('Jupiter');
 console.log(that.likedPlanet);
+//The Above code Can Be Written As 
+
+class planetShowing{
+    constructor(targetPlanet){
+        this.targetPlanet=targetPlanet;
+    }
+}
+var ans = new planetShowing('Mars');
+console.log(ans.targetPlanet);
+
