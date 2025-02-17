007a5af98462b9585505517089af863db004fdf6
Nikleshk <168631698+Nikleshk@users.noreply.github.com>
2025-02-17 13:40:42 +0000

    m

--- One_shot-2/12_import_and_export.js
+++ One_shot-2/12_import_and_export.js
@@ -1,1 +1,8 @@
-export const capitalize = str=>str.toUppercase();
+const capitalize = str=>str.toUppercase();
+
+const capitalizeString = (string)=>{
+    return string.charAt(0).toUppercase() + string.slice(1);
+}
+const foo = "bar";
+const bar = "foo";
+console.log(capitalizeString);
